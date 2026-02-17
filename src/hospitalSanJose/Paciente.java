05f82f7058f57d733b4bc50dd4371155a5c4f5f4
Karolay <luedk.garcia@autonoma.edu.co>
2026-02-17 15:31:48 -0500

    Se actualizo la clase Paciente

--- src/hospitalSanJose/Paciente.java
+++ src/hospitalSanJose/Paciente.java
@@ -1,4 +1,112 @@
 package hospitalSanJose;
 
-public class Paciente {
+/**
+ * Clase que representa a un paciente del hospital San Josè
+ *
+ */
+public class Paciente extends Persona {
+    /**
+     * Numero del historial del paciente
+     */
+    private String numHistorial;
+
+    /**
+     * Eps del paciente
+     */
+    private String eps;
+
+    /**
+     * Saldo disponible del paciente
+     */
+    private double saldoDisponible;
+
+    /**
+     * Sintomas del paciente
+     */
+    private String sintomas;
+
+    /**
+     * Constructor vacio para crear un paciente
+     */
+    public Paciente() {
+        super();
+    }
+
+    /**
+     * Constructor con parametros para crear un paciente
+     *
+     * @param nombre          Nombre del paciente
+     * @param dni             Dni del paciente
+     * @param genero          Genero del paciente
+     * @param edad            Edad del paciente
+     * @param numHistorial    Numero de historial del paciente
+     * @param eps             Eps del paciente
+     * @param saldoDisponible Saldo disponible del paciente
+     * @param sintomas        Sintomas del paciente
+     */
+    public Paciente(String nombre, String dni, String genero, int edad, String numHistorial, String eps, double saldoDisponible, String sintomas) {
+        super(nombre, dni, genero, edad);
+        this.numHistorial = numHistorial;
+        this.eps = eps;
+        this.saldoDisponible = saldoDisponible;
+        this.sintomas = sintomas;
+    }
+
+    /**
+     * Obtiene el Numero de historial del paciente
+     *
+     * @return Numero de historial del paciente
+     */
+    public String getNumHistorial() {
+        return numHistorial;
+    }
+
+    /**
+     * Obtiene tipo de eps del paciente
+     *
+     * @return tipo de eps
+     */
+    public String getEps() {
+        return eps;
+    }
+
+    /**
+     * Obtiene el saldo disponible del paciente
+     *
+     * @return Saldo disponible
+     */
+    public double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    /**
+     * Obtiene los sintomas del paciente
+     *
+     * @return Sintomas del pciente
+     */
+    public String getSintomas() {
+        return sintomas;
+    }
+
+    /**
+     * Actualiza los sintomas del paciente
+     *
+     * @param sintomas sintomas actualizados del paciente
+     */
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public boolean verificarPresupuesto(double costoConsulta) {
+        if (saldoDisponible)
+    }
+
+    /**
+     * Metodo abstracto heredado de la superclase Persona
+     * Permite al paciente presentarse con su nombre
+     */
+    @Override
+    public void presentarse() {
+        System.out.println("Soy un paciente y mi nombre es : " + getNombre());
+    }
 }
