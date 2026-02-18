f733d0da8bf6784b47f04bded004c3368cf78750
Karolay <luedk.garcia@autonoma.edu.co>
2026-02-18 11:26:22 -0500

    Se realizaron cambios en la clase medico cirujano

--- src/hospitalSanJose/MedicoCirujano.java
+++ src/hospitalSanJose/MedicoCirujano.java
@@ -2,33 +2,87 @@ package hospitalSanJose;
 
 import java.util.List;
 
+/**
+ * Clase que representa un medico cirujano del hospital San Jose
+ * Hereda de medico e implementa la interfaz ICirujano
+ */
 public class MedicoCirujano extends Medico implements ICirujano {
+    /**
+     * Numero del quirofano asignado al medico cirujano
+     */
     private int numQuirofano;
 
 
-    MedicoCirujano(String nombre, String dni, int edad, String genero, String especialidad, int numRegistro, List<Paciente> pacientesAsignados, int numQuirofano) {
+    /**
+     * Constructor para crear un medico cirujano
+     *
+     * @param nombre             Nombre del medico cirujano
+     * @param dni                DNI del medico cirujano
+     * @param edad               Edad del medico cirujano
+     * @param genero             Genero del medico cirujano
+     * @param especialidad       Especialidad del medico cirujano
+     * @param numRegistro        Numero de registro del medico cirujano
+     * @param pacientesAsignados Pacientes asignados al medico cirujano
+     * @param numQuirofano       Numero de quirofano del medico cirujano
+     */
+    public MedicoCirujano(String nombre, String dni, int edad, String genero, String especialidad, int numRegistro, List<Paciente> pacientesAsignados, int numQuirofano) {
         super(nombre, dni, edad, genero, especialidad, numRegistro, pacientesAsignados);
-        this.numQuirofano = numRegistro;
+        this.numQuirofano = numQuirofano;
     }
 
+    /**
+     * Obtiene el numero de quirofano del medico cirujano
+     *
+     * @return Numero del quirofano del medico cirujano
+     */
     public int getNumQuirofano() {
         return numQuirofano;
     }
 
+    /**
+     * Actualiza el numero de quirofano del medico cirujano
+     *
+     * @param numQuirofano Numero de quirofano del medico cirujano
+     */
     public void setNumQuirofano(int numQuirofano) {
         this.numQuirofano = numQuirofano;
     }
 
-
+    /**
+     * Metodo que valida el estado del quirofano
+     *
+     * @param estado Estado del quirofano
+     * @return True si esta disponible, false si no esta disponible
+     */
     @Override
-    public boolean validarQuirofano() {
-        
-    }
-
-    @Override
-    public boolean decidirOperacion() {
+    public boolean validarQuirofano(boolean estado) {
+        if (estado) {
+            System.out.println("El quirofano esta disponible");
+            return true;
+        }
+        System.out.println("El quirofano no esta disponible en este momento");
         return false;
     }
+
+
+    /**
+     * Metodo que aprueba o no la operacion segun la validacion del quirofano
+     *
+     * @param estado Estado del quirofano
+     * @return Un string que informa si la operacion fue aprobada o no
+     */
+    @Override
+    public String decidirOperacion(boolean estado) {
+        if (validarQuirofano(estado)) {
+            return "Operacion aprobada, el quirofano esta disponible";
+        }
+        return "No se puede realizar la operacion";
+    }
+
+
+    /**
+     * Metodo de presentacion del medico
+     */
 
     @Override
     public void presentarse() {
